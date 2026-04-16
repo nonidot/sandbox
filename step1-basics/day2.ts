7638ab9d75437f9687c42b8f5dd17496253857d5
nonidot <uninonica@gmail.com>
2026-04-16 18:19:30 +0900

    day2対応

---
+++ step1-basics/day2.ts
@@ -0,0 +1,10 @@
+// 配列
+const fruits: string[] = ["apple", "banana", "cherry"];
+const scores: number[] = [10, 20, 30];
+
+// タプル：順番と型が決まっている配列
+const dog: [string, number] = ["わんこ", 4];
+
+console.log(fruits);
+console.log(scores);
+console.log(dog);
