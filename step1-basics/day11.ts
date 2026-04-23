48c8a4b7c3b29e916bd3ecc4951837fb7eb503d1
noni <uninonica@gmail.com>
2026-04-23 21:17:23 +0900

    day11,12対応

---
+++ step1-basics/day11.ts
@@ -0,0 +1,16 @@
+// readonly：変更できないプロパティを定義する
+interface Dog {
+  readonly name: string;
+  weight: number;
+}
+
+const dog: Dog = {
+  name: "わんこ",
+  weight: 4,
+};
+
+console.log(dog);
+
+// readonlyのプロパティを変更しようとするとどうなる？
+dog.name = "ちくわ";
+dog.weight = 3;
---
+++ step1-basics/day12.ts
@@ -0,0 +1,8 @@
+// 型推論：型を書かなくてもTSが自動で型を判断してくれる
+const name = "noni";
+const weight = 4;
+const isActive = true;
+
+console.log(name);
+console.log(weight);
+console.log(isActive);
